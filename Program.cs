1e49ef9e413108c7d0691cb3dc0dae8bb9c0f3c0
lufa <lufa@itu.dk>
2025-08-29 11:15:26 +0200

    Can read csv file
    Co-authored-by: Kenny <keeg@itu.dk>
    Co-authored-by: hjfrReal <hjfr@itu.dk>
    Co-authored-by: Steinowitz <frhs@itu.dk>
    Co-authored-by: tmho <tmho@itu.dk>

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.ComponentModel;
+
+var filename = "./chirp_cli_db.csv";
+var reader = new StreamReader(filename);
+reader.ReadLine(); // to not read the header
+while (!reader.EndOfStream)
+{
+    var line = reader.ReadLine();
+
+    var firstComma = line.IndexOf(",");
+    var lastComma = line.LastIndexOf(",");
+    
+    var author = line.Substring(0,  firstComma);
+    var message = line.Substring(firstComma+2, lastComma - firstComma - 3);
+    var tempTimestamp = line.Substring(lastComma + 1);
+    var timestamp = DateTimeOffset.FromUnixTimeSeconds(long.Parse(tempTimestamp));
+    Console.WriteLine(author + " @ " + timestamp.ToString("MM/dd/yy HH:mm:ss") + ": " + message);
+    
+}
