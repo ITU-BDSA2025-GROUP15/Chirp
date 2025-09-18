11c73c5c83501d0e45dbbe2bb40b773758ed6dd4
Hjalte <hjfr@itu.dk>
2025-09-18 14:46:54 +0200

    Became singleton
    Co-Authored-By: Tobias <115621528+Cactirius@users.noreply.github.com>
    Co-Authored-By: LucasITU <189001880+LucasITU@users.noreply.github.com>
    Co-Authored-By: Frederik Stein <189008225+Steinowitz@users.noreply.github.com>
    Co-Authored-By: Kenny <51746856+kie2204@users.noreply.github.com>

--- src/SimpleDB/CSVDatabase.cs
+++ src/SimpleDB/CSVDatabase.cs
@@ -4,9 +4,40 @@ using System.Globalization;
 public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 {
     readonly string filename = "./chirp_cli_db.csv";
-    
+    private static CSVDatabase<T>? instance = null;
+    private static readonly object padlock = new object();
+
+    CSVDatabase()
+    {
+    }
+
+    public static CSVDatabase<T> Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    instance = new CSVDatabase<T>();
+                }
+                return instance;
+            }
+        }
+    }
+
+    public CSVDatabase<T> getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new CSVDatabase<T>();
+        }
+
+        return instance;
+    }
+
     // Adds cheep to database file.
-    
+
     public void Store(T record)
     {
         var fileIsNewOrEmpty = !File.Exists(filename) || new FileInfo(filename).Length == 0;
@@ -29,18 +60,19 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
     {
         var reader = new StreamReader(filename);
         var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-        
+
         var records = csv.GetRecords<T>();
-        
+
         if (limit == null) return records;
 
         var records_reverted = records.Reverse();
 
         List<T> records_new = new List<T>();
         int i = 0;
-        foreach (var record in records_reverted) {
+        foreach (var record in records_reverted)
+        {
             if (i++ >= limit) break;
-            records_new.Insert(0,record);
+            records_new.Insert(0, record);
         }
         return records_new;
     }
