4253a9116f83909bca5c25bc96fef2da736f807a
Hjalte <hjfr@itu.dk>
2025-09-18 14:26:14 +0200

    Fixed / created end 2 end unit test
    Co-Authored-By: Tobias <115621528+Cactirius@users.noreply.github.com>
    Co-Authored-By: LucasITU <189001880+LucasITU@users.noreply.github.com>
    Co-Authored-By: Frederik Stein <189008225+Steinowitz@users.noreply.github.com>
    Co-Authored-By: Kenny <51746856+kie2204@users.noreply.github.com>

--- test/Chirp.Tests/ChirpEnd2EndTest.cs
+++ test/Chirp.Tests/ChirpEnd2EndTest.cs
@@ -20,37 +20,42 @@ public class ChirpEnd2EndTest
             // Assert: Output should contain the latest cheep
             Assert.Contains("ropf", output);
             Assert.Contains("Cheeping cheeps on Chirp :)", output);
-            Assert.Contains("08/02/23 15:04:47", output);
+            Assert.True(output.Contains("08/02/23 15:04:47") || output.Contains("08/02/23 15.04.47"));
             process.WaitForExit();
         }
     }
     [Fact]
-    public void CheepCommand_CheepsCheep() {
-        using (Process process = new Process())
-        {
-            process.StartInfo.FileName = "dotnet";
-            process.StartInfo.Arguments = "run --project src/Chirp.csproj cheep LOL --dry-run";
-            process.StartInfo.WorkingDirectory = "../../../../../";
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardOutput = true;
-            
-            process.Start();
-            process.WaitForExit();
-        }
+    public void CheepCommand_CheepsCheep()
+    {
+        File.WriteAllText("chirp_cli_db.csv", @"Author,Message,Timestamp
+            ropf,""Hello, BDSA students!"",1690891760
+            adho,""Welcome to the course!"",1690978778
+            adho,""I hope you had a good summer."",1690979858
+            ropf,""Cheeping cheeps on Chirp :)"",1690981487
+        ");
 
         using (Process process = new Process())
         {
             process.StartInfo.FileName = "dotnet";
-            process.StartInfo.Arguments = "run --project src/Chirp.csproj read 1";
-            process.StartInfo.WorkingDirectory = "../../../../../";
+            process.StartInfo.Arguments = "run --project ../../../../../src/Chirp.csproj cheep LOL";
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
+
             process.Start();
-            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+        }
+        using (Process process2 = new Process())
+        {
+            process2.StartInfo.FileName = "dotnet";
+            process2.StartInfo.Arguments = "run --project ../../../../../src/Chirp.csproj read 1";
+            process2.StartInfo.UseShellExecute = false;
+            process2.StartInfo.RedirectStandardOutput = true;
+            process2.Start();
+            string output = process2.StandardOutput.ReadToEnd();
 
             // Assert: Output should contain the latest cheep
             Assert.Contains("LOL", output);
-            process.WaitForExit();
-        }
+                process2.WaitForExit();
+            }
     }
 }
