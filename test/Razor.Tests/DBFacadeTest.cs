1a2c7b6b8c748b14fe78587a6021c390a60d7b28
Kenny <keeg@itu.dk>
2025-10-03 12:36:46 +0200

    Add many unit tests, make SUT methods public
    Co-authored-by: Tobias <Cactirius@users.noreply.github.com>
    Co-authored-by: Hjalte <hjfr@itu.dk>
    Co-authored-by: Frederik Stein <Steinowitz@users.noreply.github.com>
    Co-authored-by: LucasITU <LucasITU@users.noreply.github.com>
    Co-authored-by: omar2300 <omar2300@users.noreply.github.com>

---
+++ test/Razor.Tests/DBFacadeTest.cs
@@ -0,0 +1,115 @@
+using Chirp.Razor;
+
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace Razor.Tests;
+
+[Collection("Sequential")]
+public class DBFacadeUnitTest
+{
+    public static IEnumerable<object[]> RandomNumber {
+        get {
+            Random rnd = new Random();
+            var number_list = new List<object[]>();
+
+            var count = 100;
+            for (int i = 0; i < count; i++)
+            {
+                number_list.Add([rnd.Next(1, 100)]);   
+            }
+
+            return number_list;
+        }
+    }
+
+    private void SetupTestDb()
+    {
+        var tempFilePath = Path.GetTempFileName();
+        var testDb = File.ReadAllBytes("../../../chirp-test.db");
+        File.WriteAllBytes(tempFilePath, testDb);
+        Environment.SetEnvironmentVariable("CHIRPDBPATH", tempFilePath);
+    }
+    
+    [Fact]
+    public void GetConnection_UsesDefaultDataSource()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("CHIRPDBPATH", null);
+        var tempdir = Path.Combine(Path.GetTempPath(), "chirp.db");
+        var correctDataSource = $"Data Source={tempdir}";
+
+        // Act 
+        var connection = DBFacade.GetConnection();
+        var connectionString = connection.ConnectionString;
+
+        // Assert
+        Assert.Contains(connectionString, correctDataSource);
+    }
+
+    [Theory]
+    [InlineData("/test/test.db")]
+    public void GetConnection_UsesEnvironmentVariableDataSource(string path)
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("CHIRPDBPATH", path);
+        var correctDataSource = $"Data Source={path}";
+
+        // Act 
+        var connection = DBFacade.GetConnection();
+        var connectionString = connection.ConnectionString;
+
+        // Assert
+        Assert.Contains(connectionString, correctDataSource);
+    }
+
+    [Fact]
+    public void ReadMessages_ReturnsFirstPage()
+    {
+        // Arrange
+        SetupTestDb();
+
+        // Act
+        var messages = DBFacade.ReadMessages();
+
+        // Assert
+        Assert.NotNull(messages);
+        Assert.Equal(32, messages.Count());
+    }
+
+    [Fact]
+    public void ReadMessages_WithPageArgument_ReturnsFirstPage()
+    {
+        // Arrange
+        SetupTestDb();
+
+        // Act
+        var messages = DBFacade.ReadMessages();
+        var messages_arg = DBFacade.ReadMessages(1);
+
+        // Assert
+        Assert.NotNull(messages);
+        Assert.NotNull(messages_arg);
+        Assert.Equal(messages, messages_arg);
+    }
+
+    [Theory]
+    [MemberData(nameof(RandomNumber))]
+    private void ReadMessages_ReturnsPage(int pageNumber) {
+        // Arrange
+        SetupTestDb();
+
+        // Act
+        var messages = DBFacade.ReadMessages(1,null);
+        var messages_pages = DBFacade.ReadMessages(pageNumber);
+        var messages_slice = new List<Cheep>();
+
+        var startCheep = (pageNumber - 1) * 32;
+        for (int i = startCheep; i < startCheep + 32 && i < messages.Count(); i++)
+        {
+            messages_slice.Add(messages[i]);
+        }
+
+        // Assert
+        Assert.Equal(messages_slice,messages_pages);
+    }
+}
