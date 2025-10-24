7af357efda39f0592d02b00451cf60bcf1364eff
Kenny <keeg@itu.dk>
2025-10-24 13:01:57 +0200

    Set required for author + cheep + DTO properties

--- src/Chirp.Infrastructure/Cheep.cs
+++ src/Chirp.Infrastructure/Cheep.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 public class Cheep
 {
     public int CheepId { get; set; }
-    public string Text { get; set; }
+    public required string Text { get; set; }
     public DateTime TimeStamp { get; set; }
     [Required]
     public required int AuthorId { get; set; }
