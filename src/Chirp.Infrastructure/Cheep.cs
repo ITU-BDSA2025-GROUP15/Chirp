1d968baa773ad0d9525497c1d973c381e8dc7360
omar2300 <omal@itu.dk>
2025-10-31 11:48:17 +0100

    fixed documentation. for cheeps
    this is an improved version of the attempt at documentation.

--- src/Chirp.Infrastructure/Cheep.cs
+++ src/Chirp.Infrastructure/Cheep.cs
@@ -1,12 +1,26 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 
+
+ /// <include file="../../docs/CheepDocs.xml" path="/doc/members/member[@name='T:Cheep']/*" />
 public class Cheep
 {
+    
+    /// <include file="../../docs/CheepDocs.xml" path="/doc/members/member[@name='P:Cheep.CheepId']/*" />
     public int CheepId { get; set; }
+
+    /// <include file="../../docs/CheepDocs.xml" path="/doc/members/member[@name='P:Cheep.Text']/*" />
+    [Required]
+    [StringLength(280)]
     public required string Text { get; set; }
+
+    /// <include file="../../docs/CheepDocs.xml" path="/doc/members/member[@name='P:Cheep.TimeStamp']/*" />
     public DateTime TimeStamp { get; set; }
+
+    /// <include file="../../docs/CheepDocs.xml" path="/doc/members/member[@name='P:Cheep.AuthorId']/*" />
     [Required]
     public required int AuthorId { get; set; }
+
+    /// <include file="../../docs/CheepDocs.xml" path="/doc/members/member[@name='P:Cheep.Author']/*" />
     public required Author Author { get; set; }
 }
