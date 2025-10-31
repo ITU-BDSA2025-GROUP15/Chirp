766491c556b199e1b612469addf0537de117cc15
omar2300 <omal@itu.dk>
2025-10-31 12:16:29 +0100

    author documentation done

--- src/Chirp.Infrastructure/Author.cs
+++ src/Chirp.Infrastructure/Author.cs
@@ -1,9 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 
+/// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='T:Author']/*" />
 public class Author
 {
+    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='P:Author.AuthorId']/*" />
     public int AuthorId { get; set; }
+
+    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='P:Author.Name']/*" />
     public required string Name { get; set; }
+
+    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='P:Author.Email']/*" />
     public string? Email { get; set; }
+
+    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='P:Author.Cheeps']/*" />
     public List<Cheep>? Cheeps { get; set; }
 }
