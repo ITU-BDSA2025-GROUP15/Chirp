202aa932118c379e2f3ece9eb81f04ccae2347de
Cactirius <cactirius@gmail.com>
2025-11-04 11:55:50 +0100

    Add paging buttons when viewing all cheeps
    Co-authored-by: Hjalte <hjfr@itu.dk>
    Co-authored-by: LucasITU <LucasITU@users.noreply.github.com>

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -8,6 +8,34 @@ public class PublicModel : PageModel
     private readonly ICheepService _service;
     public required List<CheepDTO> Cheeps { get; set; }
 
+
+    public int CurrentPage { get; set; }
+
+    public int PreviousPage
+    {
+        get
+        {
+            return CurrentPage - 1;
+        }
+    }
+    public int NextPage
+    {
+        get
+        {
+            if (!HasNextPage)
+            {
+                return CurrentPage;
+            }
+            else
+            {
+            return CurrentPage + 1;
+            }
+        }
+    }
+    private readonly static int defaultPageLimit = 32;
+    public bool HasNextPage { get; set; }
+    public bool HasPreviousPage { get; set; }
+
     public PublicModel(ICheepService service)
     {
         _service = service;
@@ -15,7 +43,14 @@ public class PublicModel : PageModel
 
     public ActionResult OnGet([FromQuery] int page)
     {
+        CurrentPage = page == 0 ? 1 : page;
         Cheeps = _service.GetCheeps(page);
+        HasNextPage = Cheeps.Count == defaultPageLimit ? true : false;
+        HasPreviousPage = CurrentPage != 1;
+        if (Cheeps.Count == 0)
+        {
+            return RedirectToPage("/Public");
+        }
         return Page();
     }
 }
