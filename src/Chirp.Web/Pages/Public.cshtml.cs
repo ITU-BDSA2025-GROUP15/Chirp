3aee7f0dd8e6cdad50c819a7e46751354e2e315d
Cactirius <cactirius@gmail.com>
2025-11-04 18:30:53 +0100

    Add paging buttons for author pages + PageModelLayout

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -3,51 +3,16 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Razor.Pages;
 
-public class PublicModel : PageModel
+public class PublicModel : PageModelLayout
 {
-    private readonly ICheepService _service;
-    public required List<CheepDTO> Cheeps { get; set; }
 
-
-    public int CurrentPage { get; set; }
-
-    public int PreviousPage
-    {
-        get
-        {
-            return CurrentPage - 1;
-        }
-    }
-    public int NextPage
-    {
-        get
-        {
-            if (!HasNextPage)
-            {
-                return CurrentPage;
-            }
-            else
-            {
-            return CurrentPage + 1;
-            }
-        }
-    }
-    private readonly static int defaultPageLimit = 32;
-    public bool HasNextPage { get; set; }
-    public bool HasPreviousPage { get; set; }
-
-    public PublicModel(ICheepService service)
-    {
-        _service = service;
-    }
+    public PublicModel(ICheepService service) : base(service) {}
 
     public ActionResult OnGet([FromQuery] int page)
     {
         CurrentPage = page == 0 ? 1 : page;
         Cheeps = _service.GetCheeps(page);
-        HasNextPage = Cheeps.Count == defaultPageLimit ? true : false;
-        HasPreviousPage = CurrentPage != 1;
-        if (Cheeps.Count == 0)
+        if (Cheeps.Count == 0 || page < 0)
         {
             return RedirectToPage("/Public");
         }
