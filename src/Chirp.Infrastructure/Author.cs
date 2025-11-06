2c82bb4a0034c8ff1226a2273881c49d53d86224
omar2300 <omal@itu.dk>
2025-11-06 14:03:40 +0100

    test

--- src/Chirp.Infrastructure/Author.cs
+++ src/Chirp.Infrastructure/Author.cs
@@ -1,17 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 
-/// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='T:Author']/*" />
+/// <include file="../../docs/AuthorDocs.xml" path="/doc/members/class[@name='T:Author']/*" />
 public class Author
 {
-    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='P:Author.AuthorId']/*" />
+    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/class/member[@name='P:Author.AuthorId']/*" />
     public int AuthorId { get; set; }
 
-    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='P:Author.Name']/*" />
+    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/class/member[@name='P:Author.Name']/*" />
     public required string Name { get; set; }
 
-    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='P:Author.Email']/*" />
+    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/class/member[@name='P:Author.Email']/*" />
     public string? Email { get; set; }
 
-    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/member[@name='P:Author.Cheeps']/*" />
+    /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/class/member[@name='P:Author.Cheeps']/*" />
     public List<Cheep>? Cheeps { get; set; }
 }
