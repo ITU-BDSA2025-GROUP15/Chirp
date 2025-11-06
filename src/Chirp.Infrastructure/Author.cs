30ffa323a43455b119cd8d995234f3722eb89da3
omar2300 <omal@itu.dk>
2025-11-06 14:08:34 +0100

    Update Author.cs

--- src/Chirp.Infrastructure/Author.cs
+++ src/Chirp.Infrastructure/Author.cs
@@ -14,7 +14,7 @@ public class Author
     public required string Name { get; set; }
 
     /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/class/member[@name='P:Author.Email']/*" />
-    public string? Email { get; set; }
+    //public string? Email { get; set; }
 
     /// <include file="../../docs/AuthorDocs.xml" path="/doc/members/class/member[@name='P:Author.Cheeps']/*" />
     public List<Cheep>? Cheeps { get; set; }
