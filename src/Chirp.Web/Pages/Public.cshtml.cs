18ed209fd609e16b79fdbb2f908b6a7122fef3ce
Cactirius <cactirius@gmail.com>
2025-11-07 12:07:31 +0100

    Refactoring
    Co-authored-by: LucasITU <LucasITU@users.noreply.github.com>

--- src/Chirp.Web/Pages/Public.cshtml.cs
+++ src/Chirp.Web/Pages/Public.cshtml.cs
@@ -21,10 +21,16 @@ public class PublicModel : PaginationModel
         CurrentPage = page == 0 ? 1 : page;
         Cheeps = _service.GetCheeps(CurrentPage);
     }
-    public ActionResult OnGet([FromQuery] int page)
+    public ActionResult OnGet(string author, [FromQuery] int page)
     {
         CurrentPage = page == 0 ? 1 : page;
-        Cheeps = _service.GetCheeps(page);
+        if(author != null)
+        {
+            Cheeps = _service.GetCheepsFromAuthor(author, page);
+        } else
+        {
+            Cheeps = _service.GetCheeps(page);
+        }
         if ((Cheeps.Count == 0 && CurrentPage != 1) || page < 0)
         {
             return RedirectToPage();
