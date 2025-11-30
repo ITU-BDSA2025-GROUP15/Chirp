add8100bfc3ff4b1be42d2ce8a018e2ceca550f5
Kenny <keeg@itu.dk>
2025-11-30 17:16:58 +0100

    Add styling, edit wording

--- src/Chirp.Web/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
+++ src/Chirp.Web/Areas/Identity/Pages/Account/Manage/PersonalData.cshtml.cs
@@ -11,13 +11,19 @@ namespace Chirp.Web.Areas.Identity.Pages.Account.Manage
 {
     public class PersonalDataModel : PageModel
     {
+        private readonly ICheepService _service;
         private readonly UserManager<Author> _userManager;
         private readonly ILogger<PersonalDataModel> _logger;
 
+        public required List<CheepDTO> Cheeps { get; set; }
+        public required Dictionary<string, string> PersonalData { get; set; } = new Dictionary<string, string>();
+
         public PersonalDataModel(
+            ICheepService service,
             UserManager<Author> userManager,
             ILogger<PersonalDataModel> logger)
         {
+            _service = service;
             _userManager = userManager;
             _logger = logger;
         }
@@ -29,6 +35,16 @@ namespace Chirp.Web.Areas.Identity.Pages.Account.Manage
             {
                 return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
             }
+
+            var personalDataProps = typeof(Author).GetProperties().Where(
+                                    prop => Attribute.IsDefined(prop, typeof(PersonalDataAttribute)));
+            
+            foreach (var p in personalDataProps)
+            {
+                PersonalData.Add(p.Name, p.GetValue(user)?.ToString() ?? "null");
+            }
+
+            Cheeps = _service.GetAllCheepsFromAuthor(user.Name);
 
             return Page();
         }
